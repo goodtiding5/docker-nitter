fe6c897cae9d88099b438deabcc984da15866a9e
Kenneth Zhao <kenneth.zhao@gmail.com>
2019-12-31 00:20:16 -0800

    fixing bootstrap logic and fix compose typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,27 +21,20 @@ run_nitter_program() {
 
 build_working_dir
 
-# If we have an interactive container session
-if [[ -t 0 || -p /dev/stdin ]]; then
-    if [[ $@ ]]; then 
-	eval "exec $@"
-    else 
-	export PS1='[\u@\h : \w]\$ '
-	exec /bin/sh
-    fi
-# If container is detached run nitter in the foreground
-else
-    if [ -z $1 ]; then
-	run_nitter_program
-    else
-	case "$1" in
-	    "bootstrap")
-		# workdir is prepared
-		exit 0;;
-	    "nitter")
-		run_nitter_program;;
-	    *)
-		eval "exec $@";;
-	esac
-    fi
+cmd="nitter"
+
+if [ $# -gt 0 ]; then
+    cmd=$1
 fi
+
+case "$cmd" in
+    "bootstrap")
+	# workdir is prepared by now
+	exit 0;;
+    "nitter")
+	run_nitter_program;;
+    *)
+	eval "exec $@";;
+esac
+
+exit 0

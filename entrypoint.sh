c6dc108b6439a6f3fc9feefa523fc2c2f97760e2
Kenneth Zhao <ken@epenguin.com>
2021-05-03 21:03:46 +0000

    Change parameter NITTER_TITLE to NITTER_NAME

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-NITTER_TITLE="${NITTER_TITLE:-nitter}"
+NITTER_NAME="${NITTER_NAME:-nitter}"
 NITTER_HOST="${NITTER_HOST:-nitter.net}"
 INVIDIOUS_HOST="${INVIDIOUS_HOST:-invidio.us}"
 REDIS_HOST="${REDIS_HOST:-redis}"
@@ -26,7 +26,7 @@ construct_nitter_conf()
 {
     rm -f $WORKD/nitter.conf
     cat /nitter.conf.pre > $WORKD/nitter.conf
-    sed -i "s/REDIS_HOST/$REDIS_HOST/g; s/REDIS_PORT/$REDIS_PORT/g; s/NITTER_HOST/$NITTER_HOST/g; s/NITTER_TITLE/$NITTER_TITLE/g; s/INVIDIOUS_HOST/$INVIDIOUS_HOST/g; " $WORKD/nitter.conf
+    sed -i "s/REDIS_HOST/$REDIS_HOST/g; s/REDIS_PORT/$REDIS_PORT/g; s/NITTER_HOST/$NITTER_HOST/g; s/NITTER_NAME/$NITTER_NAME/g; s/INVIDIOUS_HOST/$INVIDIOUS_HOST/g; " $WORKD/nitter.conf
 }
 
 run_nitter_program()
